fdf3c1ba6b85564b4f26a3052d6f6c65fa863d05
Rohit kumar <rky84777@gmail.com>
2025-07-26 18:28:01 +0530

    update repository

---
+++ src/main/java/com/collectionpractice/CollectionpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.collectionpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collectionpractice/CollectionpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collectionpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
