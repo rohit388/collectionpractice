5d5195f3dd7f976c6057202729eed4d32ce5590b
Rohit kumar <rky84777@gmail.com>
2025-07-30 14:09:33 +0530

    BruteForceTrialDivisionAlgoForCheckPrimeSingleValue

--- src/main/java/com/collectionpractice/basicMath/BruteForceTrialDivisionAlgoForCheckPrimeSingleValue.java
+++ src/main/java/com/collectionpractice/basicMath/BruteForceTrialDivisionAlgoForCheckPrimeSingleValue.java
@@ -2,25 +2,25 @@ package com.collectionpractice.basicMath;
 
 import java.util.Scanner;
 
-public class CheckPrimeSingleVlaueUseTrialAlgo {
+
+
+public class BruteForceTrialDivisionAlgoForCheckPrimeSingleValue {
+
     public static boolean isPrime(int n){
         if(n<=1){
             return false;
         }
-        for(int i=2;i*i<=n;i++){
+        for(int i=2;i<n;i++){
             if(n%i==0){
                 return false;
             }
         }
-
         return true;
     }
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the value for the check the prime number: ");
-        int value= sc.nextInt();
+        System.out.println("Enter the value for the check prime :");
+        int value = sc.nextInt();
         System.out.println(isPrime(value));
-
     }
 }
